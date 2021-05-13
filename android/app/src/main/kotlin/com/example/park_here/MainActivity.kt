408ed6088d2ab8937a157234a9f3453b4653eda4
Giovanna Gregório Guimarães <giovanna_gregorio@univem.edu.br>
2021-05-12 22:01:15 -0300

    Adicionando projeto

---
+++ android/app/src/main/kotlin/com/example/park_here/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.park_here
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:park_here/pages/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App Login',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
